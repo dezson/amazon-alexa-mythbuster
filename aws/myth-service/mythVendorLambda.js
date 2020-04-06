9d63d68a30d9d90c64d91cbe07222384fc3b548b
dezson <dezsonagy2@gmail.com>
2020-04-06 18:23:02 +0200

    Getting the data from a S3 bucket

--- aws/myth-service/mythVendorLambda.js
+++ aws/myth-service/mythVendorLambda.js
@@ -1,10 +1,18 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+AWS.config.update(
+    {
+         region: 'eu-west-1'
+    }
+);
+const S3 = new AWS.S3();
+
+const MYTHS_KEY = "myths.json";
+
+
 exports.get = async function(event, context, callback) {
-    const data = [
-        'first',
-        'second',
-    ];
+    const data = await getS3Object(process.env.S3BucketName, MYTHS_KEY);
 
     const mythIndex = Math.floor(Math.random() * data.length);
     const myth = data[mythIndex];
@@ -16,4 +24,22 @@ exports.get = async function(event, context, callback) {
     };
 
     callback(null, result);
+}
+
+function getS3Object(bucketName, key) {
+    const params = {
+        Bucket: bucketName,
+        Key: key
+    };
+    return new Promise((resolve, reject) => {
+        S3.getObject(params,  function(err, data) {
+            if (err) {
+                console.log(err, err.stack);
+                reject(err);
+            }
+
+            const objectData = data.Body.toString();
+            resolve(JSON.parse(objectData));
+         });
+     });
 }
