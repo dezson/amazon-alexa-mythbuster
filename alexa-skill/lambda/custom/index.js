57e027bd9d10af4acc063a90475d523773e9a5e4
dezson <dezsonagy2@gmail.com>
2020-05-05 23:53:47 +0200

    Added some new handlers

--- alexa-skill/lambda/custom/index.js
+++ alexa-skill/lambda/custom/index.js
@@ -10,14 +10,14 @@ const LaunchRequestHandler = {
     return Alexa.getRequestType(handlerInput.requestEnvelope) === "LaunchRequest";
   },
   handle(handlerInput) {
-    return handlerInput.responseBuilder.speak(welcomeMessage).reprompt(helpMessage).getResponse();
+    return handlerInput.responseBuilder.speak(messages.WELCOME).reprompt(HELP).getResponse();
   },
 };
 
-const StartHandler = {
+const GetNewMythHandler = {
   canHandle(handlerInput) {
     const request = handlerInput.requestEnvelope.request;
-    console.log("Inside StartHandler");
+    console.log("Inside GetNewMythHandler");
     console.log(JSON.stringify(request));
     return (
       request.type === "IntentRequest" &&
@@ -25,17 +25,43 @@ const StartHandler = {
     );
   },
   handle(handlerInput) {
-    console.log("Inside StartHandler - handle");
-    const attributes = handlerInput.attributesManager.getSessionAttributes();
+    console.log("Inside GetNewMythHandler - handle");
     const response = handlerInput.responseBuilder;
-    attributes.counter = 0;
-    attributes.score = 0;
+    const question = getMyth(handlerInput);
 
-    var statement = getMyth(handlerInput);
-    var speakOutput = statement;
-    var repromptOutput = statement;
+    return response.speak(question).reprompt(question).getResponse();
+  },
+};
 
-    return response.speak(speakOutput).reprompt(repromptOutput).getResponse();
+const AnswerHandler = {
+  canHandle(handlerInput) {
+    console.log("Inside AnswerHandler");
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === "IntentRequest" && request.intent.name === "AnswerIntent";
+  },
+  handle(handlerInput) {
+    console.log("Inside AnswerHandler - handle");
+    const response = handlerInput.responseBuilder;
+    var speakOutput = ``;
+    var repromptOutput = ``;
+    const item = attributes.mythItem;
+
+    const isCorrect = compareSlots(handlerInput.requestEnvelope.request.intent.slots, item.answer);
+
+    if (isCorrect) {
+      speakOutput = getSpeechCon(true);
+      handlerInput.attributesManager.setSessionAttributes(attributes);
+    } else {
+      speakOutput = getSpeechCon(false);
+    }
+
+    speakOutput += getAnswer(item);
+
+    var question = getMyth(handlerInput);
+    speakOutput += question;
+    repromptOutput = question;
+
+    return response.speak(question).reprompt(question).getResponse();
   },
 };
 
@@ -49,7 +75,7 @@ const HelpIntentHandler = {
   },
   handle(handlerInput) {
     console.log("Inside HelpIntentHandler - handle");
-    return handlerInput.responseBuilder.speak(helpMessage).reprompt(helpMessage).getResponse();
+    return handlerInput.responseBuilder.speak(messages.HELP).reprompt(messages.HELP).getResponse();
   },
 };
 
@@ -64,18 +90,41 @@ const CancelAndStopPauseIntentHandler = {
     );
   },
   handle(handlerInput) {
-    return handlerInput.responseBuilder.speak(exitSkillMessage).getResponse();
+    return handlerInput.responseBuilder.speak(messages.EXIT).getResponse();
   },
 };
 
-const SessionEndedRequestHandler = {
+const ExitHandler = {
   canHandle(handlerInput) {
-    console.log("Inside SessionEndedRequestHandler");
-    return Alexa.getRequestType(handlerInput.requestEnvelope) === "SessionEndedRequest";
+    console.log("Inside ExitHandler");
+    const request = handlerInput.requestEnvelope.request;
+
+    return (
+      request.type === `IntentRequest` &&
+      (request.intent.name === "AMAZON.StopIntent" ||
+        request.intent.name === "AMAZON.PauseIntent" ||
+        request.intent.name === "AMAZON.CancelIntent")
+    );
   },
   handle(handlerInput) {
-    console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
-    return handlerInput.responseBuilder.getResponse();
+    return handlerInput.responseBuilder.speak(message.EXIT).getResponse();
+  },
+};
+
+const RepeatHandler = {
+  canHandle(handlerInput) {
+    console.log("Inside RepeatHandler");
+    const attributes = handlerInput.attributesManager.getSessionAttributes();
+    const request = handlerInput.requestEnvelope.request;
+
+    return request.type === "IntentRequest" && request.intent.name === "AMAZON.RepeatHandler";
+  },
+  handle(handlerInput) {
+    console.log("Inside RepeatHandler - handle");
+    const attributes = handlerInput.attributesManager.getSessionAttributes();
+    const question = questionBuilder(attributes.mythItem);
+
+    return handlerInput.responseBuilder.speak(question).reprompt(question).getResponse();
   },
 };
 
@@ -85,16 +134,73 @@ const ErrorHandler = {
   },
   handle(handlerInput, error) {
     console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
-    return handlerInput.responseBuilder.getResponse();
+    return handlerInput.responseBuilder.speak(messages.ERROR).getResponse();
   },
 };
 
 /* CONSTANTS */
-const welcomeMessage = `Welcome to the Mythbuster Game! You can ask me to start the game, \
-then you are going to have  10 myths from the well known Mythbuster series and you have to \
-decide the myths are whether confirmed or busted.`;
-const helpMessage = "You can test your mythbuster skill by asking me to start the game.";
-const exitSkillMessage = "Goodbye!";
+messages = {
+  WELCOME: `Welcome to the Mythbuster Game! You can ask me to start the game, \
+    then you are going to have  10 myths from the well known Mythbuster series and you have to \
+    decide the myths are whether confirmed or busted.`,
+  HELP: "You can test your mythbuster skill by asking me to start the game.",
+  EXIT: "Goodbye!",
+  ERROR: "Sorry, an error occurred.",
+  CORREC_ANSWER: [
+    "Booya",
+    "All righty",
+    "Bam",
+    "Bazinga",
+    "Bingo",
+    "Boom",
+    "Bravo",
+    "Cha Ching",
+    "Cheers",
+    "Dynomite",
+    "Hip hip hooray",
+    "Hurrah",
+    "Hurray",
+    "Huzzah",
+    "Oh dear.  Just kidding.  Hurray",
+    "Kaboom",
+    "Kaching",
+    "Oh snap",
+    "Phew",
+    "Righto",
+    "Way to go",
+    "Well done",
+    "Whee",
+    "Woo hoo",
+    "Yay",
+    "Wowza",
+    "Yowsa",
+  ],
+  WRONG_ANSWER: [
+    "Argh",
+    "Aw man",
+    "Blarg",
+    "Blast",
+    "Boo",
+    "Bummer",
+    "Darn",
+    "D'oh",
+    "Dun dun dun",
+    "Eek",
+    "Honk",
+    "Le sigh",
+    "Mamma mia",
+    "Oh boy",
+    "Oh dear",
+    "Oof",
+    "Ouch",
+    "Ruh roh",
+    "Shucks",
+    "Uh oh",
+    "Wah wah",
+    "Whoops a daisy",
+    "Yikes",
+  ],
+};
 
 /* HELPER FUNCTIONS */
 function getMyth(handlerInput) {
@@ -104,22 +210,55 @@ function getMyth(handlerInput) {
 
   const attributes = handlerInput.attributesManager.getSessionAttributes();
   attributes.mythItem = mythItem;
-  attributes.counter += 1;
-  handlerInput.attributesManager.setSessionAttributes(attributes);
-  return questionBuilder(attributes.counter, mythItem);
+  // attributes.counter += 1;
+  // handlerInput.attributesManager.setSessionAttributes(attributes);
+  return questionBuilder(mythItem);
+  // return questionBuilder(attributes.counter, mythItem);
 }
 
-function questionBuilder(counter, item) {
-  return `Here is your ${counter}th question. ${item.Statement}. What do you think, is it CONFIRMED or PLAUSIBLE?`;
+function questionBuilder(item) {
+  // return `Here is your ${counter}th question. ${item.Statement}. What do you think, is it CONFIRMED or PLAUSIBLE?`;
+  return `Here is your  question. ${item.Statement}. What do you think, is it CONFIRMED or PLAUSIBLE?`;
+}
+
+function compareSlots(slots, value) {
+  for (const slot in slots) {
+    if (Object.prototype.hasOwnProperty.call(slots, slot) && slots[slot].value !== undefined) {
+      if (slots[slot].value.toLowerCase() === value.toLowerCase()) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getSpeechCon(type) {
+  if (type)
+    return `<say-as interpret-as='interjection'>${
+      messages.CORREC_ANSWER[getRandom(0, messages.CORREC_ANSWER.length - 1)]
+    }! </say-as><break strength='strong'/>`;
+  return `<say-as interpret-as='interjection'>${
+    WRONG_ANSWER[getRandom(0, WRONG_ANSWER.length - 1)]
+  } </say-as><break strength='strong'/>`;
+}
+
+function getAnswer(item) {
+  return item.explanation;
 }
 
 exports.handler = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
     LaunchRequestHandler,
     HelpIntentHandler,
-    StartHandler,
+    GetNewMythHandler,
+    AnswerHandler,
+    RepeatHandler,
     CancelAndStopPauseIntentHandler,
-    SessionEndedRequestHandler
+    ExitHandler
   )
   .addErrorHandlers(ErrorHandler)
   .lambda();
